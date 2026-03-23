b18dbd365d0d914ba5f4d695dd6d6f5bd62110dc
Emir <59117907+mehmetemirsari@users.noreply.github.com>
2026-03-24 00:53:28 +0300

    Update MovieAdapter.java

--- app/src/main/java/com/example/week10/MovieAdapter.java
+++ app/src/main/java/com/example/week10/MovieAdapter.java
@@ -1,7 +1,6 @@
 package com.example.week10;
 
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
@@ -9,33 +8,55 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
+/**
+ * RecyclerView adapter for displaying movie items.
+ */
 public class MovieAdapter extends RecyclerView.Adapter<MovieViewHolder> {
-    private List<Movie> movies;
 
-    public MovieAdapter(List<Movie> movies) {
-        this.movies = movies;
+    private final List<Movie> movieList;
+
+    /**
+     * Creates a new adapter with the given movie list.
+     *
+     * @param movieList the list of movies to display
+     */
+    public MovieAdapter(@NonNull List<Movie> movieList) {
+        this.movieList = movieList;
     }
 
+    /**
+     * Creates a new ViewHolder for a movie item.
+     *
+     * @param parent the parent ViewGroup
+     * @param viewType the type of view
+     * @return a new MovieViewHolder
+     */
     @NonNull
     @Override
     public MovieViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.item_movie, parent, false);
-        return new MovieViewHolder(view);
+        return new MovieViewHolder(
+                LayoutInflater.from(parent.getContext()).inflate(R.layout.item_movie, parent, false)
+        );
     }
 
+    /**
+     * Binds movie data to the ViewHolder at the given position.
+     *
+     * @param holder the ViewHolder to bind data to
+     * @param position the position of the item in the list
+     */
     @Override
     public void onBindViewHolder(@NonNull MovieViewHolder holder, int position) {
-        holder.bind(movies.get(position));
+        holder.bind(movieList.get(position));
     }
 
+    /**
+     * Returns the total number of movie items.
+     *
+     * @return the item count
+     */
     @Override
     public int getItemCount() {
-        return movies.size();
-    }
-
-    public void updateMovies(List<Movie> movies) {
-        this.movies = movies;
-        notifyDataSetChanged();
+        return movieList.size();
     }
 }
